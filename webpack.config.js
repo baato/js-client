5d15ce33ad5389719b979cf493fc698d852edcf4
Arogya Koirala <arogyakoirala@gmail.com>
2020-08-10 18:37:20 +0545

    updated build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: __dirname + "/dist/js", // or path: path.join(__dirname, "dist/js"),
+    path: __dirname + "/dist",
     filename: "bundle.js",
     library: "Baato",
     libraryTarget: "umd",
-    umdNamedDefine: true,
+    libraryExport: "default"
   },
 };
