bb65737e738bb85edd022a9edad4f2804c5060c3
Arogya Koirala <arogyakoirala@gmail.com>
2020-11-27 10:55:06 +0545

    Updated reverse to add new API parameter

--- src/BaatoReverseSearch.js
+++ src/BaatoReverseSearch.js
@@ -4,10 +4,11 @@ class BaatoReverseSearch {
     constructor(
         props,
     ) {
-        this.key = props && props.key ? props.key : 'YOURQUERY'        
+        this.key = props && props.key ? props.key : 'YOURQUERY'
         this.radius = props && props.radius ? props.radius : 0.5 // based on spring-boot API design
         this.baseUrl = props && props.baseUrl ? props.baseUrl : 'https://api.baato.io/api'
         this.apiVersion = props && props.apiVersion ? props.apiVersion : '1'
+        this.limit = props && props.limit ? props.limit : '1'
     }
 
     setKey(key) {
@@ -21,10 +22,15 @@ class BaatoReverseSearch {
     }
 
     setCoordinates(coordinates) {
-      this.lat = coordinates[0];
-      this.lon = coordinates[1];
-      return this;
+        this.lat = coordinates[0]
+        this.lon = coordinates[1]
+        return this
     }
+
+    setLimit(limit) {
+        this.limit = String(limit)
+    }
+
 
     // setLat(lat) {
     //     this.lat = lat
@@ -55,6 +61,7 @@ class BaatoReverseSearch {
                     lat: this.lat,
                     lon: this.lon,
                     radius: this.radius,
+                    limit: this.limit,
                 },
             })
                 .then(response => response.data)
