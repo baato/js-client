b4f0bb87a3922973ff89a2dc5542494c1f8eb52f
drklrd <srvbhattarai@gmail.com>
2022-03-16 15:11:56 +0545

    update webpack config and dependencies for IE11 compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,59 @@
+const path = require('path')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const webpack = require('webpack')
+
+const env = process.env.WEBPACK_ENV
+
+// Simply configure those 4 variables:
+const JS_SOURCE_FILES = ['babel-polyfill', './src/index.js']
+const OUTPUT_FILENAME = 'bundle'
+const DEST_FOLDER = 'dist'
+const COPYRIGHT = 'Add your copyright here. It is included at the beginning of your bundle.'
+
+const OUTPUT_FILE = `${OUTPUT_FILENAME}.js`
+const OUTPUT_FILE_MIN = `${OUTPUT_FILENAME}.min.js`
+
+const { plugins, outputfile, mode } = env == 'build'
+  ? {
+    plugins: [
+      new UglifyJSPlugin(),
+      new webpack.BannerPlugin(COPYRIGHT),
+    ],
+    outputfile: OUTPUT_FILE_MIN,
+    mode: 'production',
+  }
+  : {
+    plugins: [
+      new webpack.BannerPlugin(COPYRIGHT),
+    ],
+    outputfile: OUTPUT_FILE,
+    mode: 'development',
+  }
+
 module.exports = {
-  entry: "./src/index.js",
+  mode,
+  entry: JS_SOURCE_FILES,
   output: {
-    path: __dirname + "/dist",
-    filename: "bundle.js",
-    library: "Baato",
-    libraryTarget: "umd",
-    libraryExport: "default",
+    path: path.join(__dirname, DEST_FOLDER),
+    filename: outputfile,
+    library: 'Baato',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    umdNamedDefine: true,
   },
-};
+  module: {
+    rules: [{
+      // Only run `.js` files through Babel
+      test: /\.m?js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+    }],
+  },
+  // devtool: 'source-map',
+  plugins,
+}
